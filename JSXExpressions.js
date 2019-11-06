3b23a443ac33920a26c114a4705c32c55c665be6
mshakirkhan <mmshakirkhan@yahoo.com>
2019-11-06 16:18:10 +0500

    Create JSXExpressions.js

---
+++ JSXExpressions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+     <h1>First Application on React</h1>
+     <p>This is paragraph One</p>
+     <p>This is paragraph Two</p>
+     <p>This is paragraph Three</p>
+     <p>This is paragraph Four</p>
+    </div>
+  );
+}
+
+export default App;
